88d59e7a071600dca8c0f449791e8d8c515bc8e1
Nabin-Kndl <nabinkndl577@gmail.com>
2025-02-27 16:01:03 +0545

    all the required changes for light mode

--- ad-tech-frontend/src/app/components/ui/sidebar.tsx
+++ ad-tech-frontend/src/app/components/ui/sidebar.tsx
@@ -16,15 +16,15 @@ export default function Sidebar({ selectedTab, setSelectedTab }: SidebarProps) {
       {/* Sidebar Toggle Button */}
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="fixed top-4 left-4 z-50 p-1 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50"
+        className="fixed top-4 left-4 z-50 p-1  text-black rounded-md"
       >
-        {isOpen ? <X size={32} className="text-black rounded-2xl p-1" /> : <Menu size={32} />}
+        {isOpen ? <X size={32} className="bg-[#D1D5DB] text-black rounded-2xl p-1" /> : <Menu size={32} />}
       </button>
       {/* Sidebar and Content Wrapper */}
       <div className={`h-screen flex transition-all duration-300 ${isOpen ? "ml-64" : "ml-1"}`}>
         {/* Sidebar */}
         <div
-          className={`h-screen bg-[#0D2B45] text-white transition-all duration-300 ${
+          className={`h-screen bg-[#D1D5DB] text-white transition-all duration-300 ${
             isOpen ? "w-64 p-4" : "w-0 overflow-hidden"
           } fixed top-0 left-0 shadow-lg`}
         >
@@ -33,7 +33,7 @@ export default function Sidebar({ selectedTab, setSelectedTab }: SidebarProps) {
               <li>
                 <button
                   onClick={() => setSelectedTab("asin")}
-                  className={`w-full text-left py-2 px-4 rounded text-white ${
+                  className={`w-full text-left py-2 px-4 rounded text-black ${
                     selectedTab === "asin" ? "bg-gray-600" : "hover:bg-[#1B3A57]"
                   }`}
                 >
@@ -43,7 +43,7 @@ export default function Sidebar({ selectedTab, setSelectedTab }: SidebarProps) {
               <li>
                 <button
                   onClick={() => setSelectedTab("keywordPerformance")}
-                  className={`w-full text-left py-2 px-4 rounded text-white ${
+                  className={`w-full text-left py-2 px-4 rounded text-black ${
                     selectedTab === "keywordPerformance" ? "bg-gray-600" : "hover:bg-[#1B3A57]"
                   }`}
                 >
@@ -53,7 +53,7 @@ export default function Sidebar({ selectedTab, setSelectedTab }: SidebarProps) {
               <li>
                 <button
                   onClick={() => setSelectedTab("NegativeKeyword")}
-                  className={`w-full text-left py-2 px-4 rounded text-white ${
+                  className={`w-full text-left py-2 px-4 rounded text-black ${
                     selectedTab === "NegativeKeyword" ? "bg-gray-600" : "hover:bg-[#1B3A57]"
                   }`}
                 >
@@ -63,7 +63,7 @@ export default function Sidebar({ selectedTab, setSelectedTab }: SidebarProps) {
               <li>
                 <button
                   onClick={() => setSelectedTab("keywordRecommendation")}
-                  className={`w-full text-left py-2 px-4 rounded text-white ${
+                  className={`w-full text-left py-2 px-4 rounded text-black ${
                     selectedTab === "keywordRecommendation" ? "bg-gray-600" : "hover:bg-[#1B3A57]"
                   }`}
                 >
